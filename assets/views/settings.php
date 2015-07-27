611d126057491984baf59af63c6fc63aacb350a3
WWGate <WWGate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 06:33:15 +0000

    right column in setting page
    git-svn-id: https://plugins.svn.wordpress.org/wp-cloaker/trunk@1207206 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/views/settings.php
+++ assets/views/settings.php
@@ -4,6 +4,20 @@ if (!current_user_can('manage_options')) {
 }
 $path = str_replace('assets'.DIRECTORY_SEPARATOR.'views','',plugin_dir_path( __FILE__ ));
 ?>
+<style>
+	.rightcol{
+		position:absolute;
+		right:0;
+		top:115px;
+		width:250px;	
+	}
+	.rightcol h3{
+		line-height:30px;	
+	}
+	.rightcol img{
+		max-width:100%;
+	}
+</style>
 <div class="wrap">
 	<img class="wp-cloaker-logo" src="<?php echo  plugins_url( 'images/wp-cloaker-logo.png', __FILE__ ) ?>" alt="WP Cloaker Logo" />
 	<h2><?php echo __('WP Cloaker Default Settings');?></h2>
@@ -12,4 +26,12 @@ $path = str_replace('assets'.DIRECTORY_SEPARATOR.'views','',plugin_dir_path( __F
         <?php do_settings_sections( 'wp_cloaker_link_settings' ); ?>
         <?php submit_button(); ?>
     </form>
+    <div class="rightcol">
+        <h3>do you have any WordPress project?</h3><br>
+        <a href="https://www.upwork.com/users/~01cdee61686dcfce3c" target="_blank" class="hireus">
+        	<img src="<?php echo plugin_dir_url(__FILE__).'images/hire-us.png' ?>" />
+        </a>
+        <p style="text-align:center"><strong>Hourly rate: $22.22</strong></p><br>
+        <a href="http://www.wwgate.net" target="_blank">Developed by WWGate</a>
+    </div>
 </div>
